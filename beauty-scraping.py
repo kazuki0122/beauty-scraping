2b17db6213c6c52e358744105a8339f580c72b21
kazuki0122 <kazuki.tsujimura@spacemarket.co.jp>
2023-07-17 14:37:00 +0900

    feat: サロン詳細まで遷移させることを実装

--- beauty-scraping.py
+++ beauty-scraping.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
-driver = webdriver.Chrome()
+from selenium.webdriver.common.by import By
 
-driver.get('https://beauty.hotpepper.jp/svcSA/macAD/')
+driver = webdriver.Chrome()
+driver.get('https://beauty.hotpepper.jp/svcSA/macAD/salon/')
+elements = driver.find_elements(By.CLASS_NAME,"message")
+
+elemnt = elements[1].find_element(By.TAG_NAME, 'a')
+elemnt.click()
